cf14cfcce0a26825f3af7cb46b0f1071059dca4f
Bertrand B <boyer.bertrand@gmail.com>
2018-06-05 15:47:16 +0200

    Quick refactoring.

--- bonjourmadame.sh
+++ bonjourmadame.sh
@@ -5,33 +5,50 @@ BM_URL="http://www.bonjourmadame.fr/"
 
 DEFAULT_FOLDER="$HOME/Images/BM";
 DEFAULT_NAME=BM-`date +%F`;
+
 CONNECTED=`ping -c 1 google.com > /dev/null 2>&1 && echo $?`;
+TARGET_FOLDER=$DEFAULT_FOLDER;
 
-bm () {
+usage () {
+    echo "Usage: $(basename "$0") { -c | -i } [ target name ]";
+    echo "-c, --cron:   Add a crontab entry every weekdays at 10:30AM."
+    echo "-i, --image:  Donwload the daily Madame."
+    echo "-h, --help:   Display usage."
+}
+
+prepare_folder() {
+  if [ "$1" = "." ]; then
+    TARGET_FOLDER=`pwd`;
+  fi
+
+  if [ ! -d "$TARGET_FOLDER" ]; then
+    mkdir -p $TARGET_FOLDER;
+  fi
+}
+
+dl() {
+  if [ -z "$2" ]; then
+    IMG_NAME=$DEFAULT_NAME;
+  else
+    IMG_NAME=$2;
+  fi
+
+  IMG_URL=`wget -O - -q $1 | grep -Eo "(http[s]?://[0-9]+.media.tumblr.com/[0-9a-f]*/tumblr[^\"]+)" | head -n 1` ;
+  IMG_EXTENSION="${IMG_URL##*.}";
+  IMG_PATH=$TARGET_FOLDER/$IMG_NAME.$IMG_EXTENSION;
+
+  if [ ! -f "$IMG_PATH" ]; then
+    wget -nv $IMG_URL -O $IMG_PATH;
+  else
+    echo "File [$IMG_PATH] already exists.";
+  fi;
+}
+
+
+bm_single () {
+  prepare_folder $1;
   if [ "0" -eq "$CONNECTED" ] ; then
-    if [ -z "$1" ]; then
-      TARGET_FOLDER=$DEFAULT_FOLDER;
-    elif [ "$1" = "." ]; then
-      TARGET_FOLDER=`pwd`;
-    fi
-
-    if [ -z "$2" ]; then
-      IMG_NAME=$DEFAULT_NAME;
-    fi
-
-    if [ ! -d "$TARGET_FOLDER" ]; then
-      mkdir -p $TARGET_FOLDER;
-    fi
-
-    IMG_URL=`wget -O - -q $BM_URL | grep -Eo "(http[s]?://[0-9]+.media.tumblr.com/[0-9a-f]*/tumblr[^\"]+)" | head -n 1` ;
-    IMG_EXTENSION="${IMG_URL##*.}";
-    IMG_PATH=$TARGET_FOLDER/$IMG_NAME.$IMG_EXTENSION;
-
-    if [ ! -f "$IMG_PATH" ]; then
-      wget -nv $IMG_URL -O $IMG_PATH;
-    else
-      echo "File [$IMG_PATH] already exists.";
-    fi;
+    dl $BM_URL $2;
   else
     echo "There is no internet connection.";
   fi
@@ -48,20 +65,13 @@ bm_cron() {
   echo "Cron created: $CRON";
 }
 
-usage () {
-    echo "Usage: $(basename "$0") { -c | -i } [ target name ]";
-    echo "-c, --cron:   Add a crontab entry every weekdays at 10:30AM."
-    echo "-i, --image:  Donwload the daily Madame."
-    echo "-h, --help:   Display usage."
-}
-
 run() {
   case "$1" in
     -c | --cron)
       bm_cron $0;
     ;;
     -i | --image)
-      bm $2 $3;
+      bm_single $2 $3;
     ;;
     *)
       usage;
